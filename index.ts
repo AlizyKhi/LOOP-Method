ae06eee8a4908d18247fa96aa876b5a82b4bcf40
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-05-25 17:55:03 +0500

    Add files via upload
    LOOP METHOD 
    How To Make Loop In The Array?

---
+++ index.ts
@@ -0,0 +1,35 @@
+for(let j=3;j<6;j++){
+console.log("statement"+j);
+}
+
+for (let i=0;i<3;i++){
+    console.log("statement"+i);
+}
+
+for (var a=100;a<110;a++){
+    console.log("statement"+a);
+}
+
+for (let l=89;l<90;l++){
+    console.log("statement"+l);
+}
+                                             //LOOP IN THE ARRAY//
+let students:string[]=["Hafsa","Shayan","Ali","Bisma"]
+for (let i=0;i<students.length;i++) {
+    console.log (students[i]);
+}                           
+
+var glasses:string[]=["SunlightGlass","BlackGlass","WhiteGlass"]
+for (var i=0;i<glasses.length;i++) {
+    console.log (glasses[i]);
+}
+
+let cups:string[]=["Steal","Mirror","Sensitive","Hard","Plastic"]
+for (let i=0;i<cups.length;i++) {
+    console.log(cups[i]);
+}
+
+var relations:string[]=["father","mother","daughter","son","sister","brother","wife","husband"]
+for (var i=0;i<relations.length;i++) {
+    console.log (relations[i]);
+}
